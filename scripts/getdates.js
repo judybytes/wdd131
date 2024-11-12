b63d13df8d9182e9af6a3f9bfa2ebdd52286db77
judybytes <mot24010@byui.edu>
2024-11-12 03:12:37 +0200

    week 1 assignment done

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Get current year for footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get last modified date for footer
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
