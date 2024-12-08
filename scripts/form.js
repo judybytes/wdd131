22de50cc323e47db77cce539aaa594b346d78886
judybytes <mot24010@byui.edu>
2024-12-08 17:52:39 +0200

    fixed forms errors

--- scripts/form.js
+++ scripts/form.js
@@ -1,10 +1,10 @@
 // Product Array
 const products = [
-    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+    { id: "fc-1888", name: "Flux Capacitor", averageRating: 4.5 },
+    { id: "fc-2050", name: "Power Laces", averageRating: 4.7 },
+    { id: "fs-1987", name: "Time Circuits", averageRating: 3.5 },
+    { id: "ac-2000", name: "Low Voltage Reactor", averageRating: 3.9 },
+    { id: "jj-1969", name: "Warp Equalizer", averageRating: 5.0 }
 ];
 
 // Populate Product Name Dropdown
@@ -18,8 +18,7 @@ products.forEach(product => {
 
 // Track review count with localStorage
 const form = document.getElementById("product-review-form");
-form.addEventListener("submit", () => {
+form.addEventListener("submit", (e) => {
     let reviewCount = parseInt(localStorage.getItem("reviewCount")) || 0;
     localStorage.setItem("reviewCount", reviewCount + 1);
 });
-
